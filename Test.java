c24669881312407377395fbe880b2ac3b782ee0d
Matías Exequiel Borra Quiroz <145360448+CleanMatyx@users.noreply.github.com>
2023-11-24 16:05:48 +0100

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,9 @@
+public class Test
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello");
+        System.out.println("Have a nice day!");
+        System.out.println("And learn a lot of Java :-)");
+    }
+}
